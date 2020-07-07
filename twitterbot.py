07e08ca33e926a50a8169cea2e48caf602eccf84
Drew Bornt <andrew.bornt@gmail.com>
2020-07-07 07:48:43 -0400

    Add files via upload

---
+++ twitterbot.py
@@ -0,0 +1,25 @@
+import tweepy;
+import time;
+
+auth = tweepy.OAuthHandler('API_KEY','API_SECRET_KEY')
+auth.set_access_token('ACCESS_TOKEN','ACCESS_TOKEN_SECRET')
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search = "100daysofcode" # This is the term that we want to search
+numTweets = 500 # this is the number of tweets that we want to aggregate through
+
+for tweet in tweepy.Cursor(api.search, search).items(numTweets):
+    try:
+        print ('Tweet Liked')
+        tweet.favorite()
+        tweet.repeat
+        time.sleep(87) # 1000 tweets liked / day, so 1 tweet liked every 87 seconds
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+
+    except StopIteration:
+        stop
